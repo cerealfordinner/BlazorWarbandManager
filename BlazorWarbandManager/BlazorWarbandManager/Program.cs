064db7f0b2debfb69c34f12f294408ca86472342
cerealfordinner <cerealfordinnerx@gmail.com>
2023-01-15 22:16:20 -0500

    Switch to MudBlazor and Add LocalStorage Library, removed leftover css and Bootstrap

--- BlazorWarbandManager/BlazorWarbandManager/Program.cs
+++ BlazorWarbandManager/BlazorWarbandManager/Program.cs
@@ -1,13 +1,17 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWarbandManager;
 using BlazorWarbandManager.Services;
+using MudBlazor.Services;  
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddTransient<ItemService>();
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddHttpClient<IItemService, ItemService>(client =>
 {
     client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
