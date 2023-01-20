dda8838afa7317725f865cafbdadbcdcd694c244
cerealfordinner <cerealfordinnerx@gmail.com>
2023-01-19 21:08:49 -0500

    Tweaks

--- BlazorWarbandManager/BlazorWarbandManager/Models/WarbandMember.cs
+++ BlazorWarbandManager/BlazorWarbandManager/Models/WarbandMember.cs
@@ -7,6 +7,7 @@ public class WarbandMember
     public string Name { get; set; }
     public string Class { get; set; }
     public List<Ability> Abilities { get; set; }
+    public Dictionary<string, int> Stats { get; set; }
     public int Health { get; set; }
     public List<Item> Items { get; set; }
 }
